6e9abab0ebeaaa1650ede021813787e93277a8bc
Juan Cortes <j.cortes@crowdfarming.com>
2021-12-07 13:34:23 +0100

    feat: create singleton pattern example

---
+++ src/singleton.ts
@@ -0,0 +1,33 @@
+import { BaseRecord, Recipe, Database } from "./models";
+
+function createDatabase<T extends BaseRecord>() {
+  class InMemoryDatabase implements Database<T> {
+    private db: Record<string, T> = {};
+
+    static instance: InMemoryDatabase = new InMemoryDatabase();
+
+    public set(newValue: T): void {
+      this.db[newValue.id] = newValue;
+    }
+
+    public get(id: string): T {
+      return this.db[id];
+    }
+  }
+
+  // Singleton pattern
+  // const db = new InMemoryDatabase();
+  // return db;
+  return InMemoryDatabase;
+}
+
+const RecipeDB = createDatabase<Recipe>();
+
+RecipeDB.instance.set({
+  id: "aadwq223",
+  difficulty: "easy",
+  kcal: 234,
+  name: "espaguetis carbonara",
+});
+
+console.log(RecipeDB.instance.get("aadwq223"));
